5a35bf2f4c698d17d3c37093c8009477a542a527
D.Herboldshimer <dustnpdx@gmail.com>
2016-10-25 00:17:15 -0700

    created main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by DuHerb on 10/24/16.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
